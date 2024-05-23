8fb6ca5181c742a069cdf71f7edb373f1e991f0e
David Zarzavilla Borrego <dzarzavillaborrego@uma.es>
2024-05-23 17:02:45 +0200

    Update Navbar

--- casino/src/components/Navbar.jsx
+++ casino/src/components/Navbar.jsx
@@ -11,7 +11,6 @@ function Navbar() {
 
   setWalletMoney = setWalletAmount;
 
-  //funcion para obtener el dinero
   useEffect(() => {
     fetchWalletAmount();
   }, []);
@@ -28,10 +27,9 @@ function Navbar() {
     }
   };
 
-  //funcion para cerrar sesion
   const handleLogout = () => {
     localStorage.removeItem("isLoggedIn");
-    localStorage.removeItem("userId"); // Limpiar el ID del usuario
+    localStorage.removeItem("userId");
     setWalletAmount(0);
     window.location.href = "/";
   };
@@ -41,7 +39,7 @@ function Navbar() {
       className="navbar navbar-expand-lg navbar-light py-0"
       style={{ backgroundColor: "#E4A700" }}
     >
-      <Link to={"/"} className="navbar-brand text-decoration-none text-dark ms-2">
+      <Link to="/" className="navbar-brand text-decoration-none text-dark ms-2">
         <img src={StarlightLogo} alt="Volver a inicio" />
       </Link>
       <button
@@ -51,7 +49,7 @@ function Navbar() {
         data-bs-target="#navbarNav"
         aria-controls="navbarNav"
         aria-expanded="false"
-        aria-label="Volver a inicio"
+        aria-label="Toggle navigation"
         tabIndex={0}
       >
         <span className="navbar-toggler-icon"></span>
@@ -62,65 +60,79 @@ function Navbar() {
       >
         <ul className="navbar-nav">
           <li className="nav-item">
-            <button className="btn btn-sm btn-outline-light position-absolute end-0 bottom-0 mb-2 me-2" tabIndex={-1} aria-label="Ayuda"
-            style={{padding: "0.1rem", fontFamily: "Outfit", fontSize:"15px", fontWeight: "600"}}>
-              <Link to={"/ayuda"} className="nav-link fw-bold text-dark" tabIndex={0}> 
-                Ayuda <BsInfoCircle />
+            <button
+              className="btn btn-sm btn-outline-light end-0 ms-2 bottom-0 mb-2 me-2"
+              tabIndex={-1}
+              aria-label="Ayuda"
+            >
+              <Link
+                to="/ayuda"
+                className="nav-link text-dark fw-bold d-flex align-items-center"
+                aria-label="Ayuda"
+              >
+                Ayuda <BsInfoCircle className="ms-1" />
               </Link>
             </button>
           </li>
         </ul>
         <div className="navbar-nav">
-          <div className="d-flex flex-column align-items-center">
-            <div style={{fontFamily: "Outfit", fontWeight: "500"}}>
+          <div className="d-flex flex-column align-items-center text-center text-lg-start">
+            <div style={{ fontFamily: "Outfit", fontWeight: "500" }}>
               <p className="fs-4 mb-0 mt-0">Monedero</p>
-              <div className="d-flex align-items-center">
+              <div className="d-flex align-items-center justify-content-center justify-content-lg-start">
                 <div className="bg-transparent text-black fs-5 px-4 py-2 rounded border border-1 border-dark me-2">
                   {walletAmount.toFixed(2)}€
                 </div>
-                <Link to={"/shop"} className="text-white text-decoration-none h1" tabIndex={0}>
+                <Link
+                  to="/shop"
+                  className="text-white text-decoration-none h1"
+                  tabIndex={0}
+                >
                   <button
                     className="btn btn-primary btn-outline-light"
                     tabIndex={-1}
                     aria-label="Tienda"
                     style={{
                       backgroundColor: "#A46928",
-                      border: "2px solid #A46928"
+                      border: "2px solid #A46928",
                     }}
                   >
                     +
                   </button>
-              </Link>
-
+                </Link>
               </div>
             </div>
           </div>
         </div>
-        <div style={{ alignSelf: "flex-start", fontFamily: "Outfit", fontWeight: "700"}}>
+        <div className="d-flex flex-column flex-lg-row align-items-center mt-2 mt-lg-0">
           {localStorage.getItem("isLoggedIn") ? (
             <button
-              className="btn position-absolute end-0 top-0"
+              className="btn me-2 mb-2 "
               style={{ backgroundColor: "#BB9D0A" }}
               onClick={handleLogout}
-              tabIndex={0}
               aria-label="Cerrar sesión"
             >
               Cerrar sesión
             </button>
           ) : (
             <>
-              <button 
-                className="btn" 
-                aria-label="Inicio de sesión" 
+              <button
+                className="btn"
+                aria-label="Inicio de sesión"
                 style={{ backgroundColor: "#BB9D0A" }}
                 tabIndex={-1}
+              >
+                <Link
+                  to={"/login"}
+                  className="text-white text-decoration-none"
+                  tabIndex={0}
                 >
-                <Link to={"/login"} className="text-white text-decoration-none" tabIndex={0}>
                   Inicio de sesión
                 </Link>
               </button>
               <button
-                className="btn" aria-label="Registro" 
+                className="btn mt-2 mb-2"
+                aria-label="Registro"
                 style={{
                   marginLeft: "10px",
                   marginRight: "10px",
--- casino/src/pages/Shop.jsx
+++ casino/src/pages/Shop.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import ShopOpciones from "../components/ShopOpciones";
+import Footer from "../components/Footer";
 
 function Shop() {
   return (
     <>
       <div style={{ backgroundColor: "#282828", minHeight: "100vh" }}>
-      <Navbar />
-      <div>
-        <div className="mt-5">
-          <ShopOpciones />
+        <Navbar />
+        <div>
+          <div className="mt-5 mb-2">
+            <ShopOpciones />
+          </div>
         </div>
       </div>
-    </div>
-   
+      <Footer />
     </>
   );
 }
