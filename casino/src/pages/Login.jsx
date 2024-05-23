5e0503cc570e7668594b42ba4258270843c4d77d
David Zarzavilla Borrego <dzarzavillaborrego@uma.es>
2024-05-23 12:17:39 +0200

    Update Login.jsx

--- casino/src/pages/Login.jsx
+++ casino/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { HashRouter } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { FaEye } from "react-icons/fa";
 import { MdCancel } from "react-icons/md";
 import Validation from "../api/LoginValidation";
@@ -12,7 +12,8 @@ function Login() {
   });
   const [errors, setErrors] = useState({});
   const [showPassword, setShowPassword] = useState(false);
-  const navigate = HashRouter();
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -32,22 +33,33 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     const formErrors = Validation(formData);
     setErrors(formErrors);
+
     if (Object.values(formErrors).every((error) => !error)) {
       try {
         const response = await axios.post(
           "http://localhost:8081/login",
           formData
         );
+
         localStorage.setItem("isLoggedIn", true);
         localStorage.setItem("userId", response.data.userId);
         navigate("/");
       } catch (error) {
-        if (error.response && error.response.status === 401) {
-          setErrors({ login: "Correo o contraseña incorrectos" });
-        } else {
-          console.error("Error en la solicitud:", error);
+        if (error.response) {
+          if (error.response.status === 401) {
+            if (error.response.data.error === "Contraseña incorrecta") {
+              setErrors({ login: "Contraseña incorrecta" }); // Contraseña incorrecta
+            } else if (error.response.data.error === "Correo incorrecto") {
+              setErrors({ login: "Correo incorrecto" }); // Correo incorrecto
+            } else {
+              setErrors({ login: "Correo o contraseña incorrectos" });
+            }
+          } else {
+            console.error("Error en la solicitud:", error);
+          }
         }
       }
     }
@@ -60,7 +72,7 @@ function Login() {
           <div className="card bg-transparent border-1 border-light">
             <div className="card-body">
               <div className="d-flex justify-content-between align-items-center mb-4">
-                <h2 className="card-title text-white">Iniciar sesión</h2>
+                <h2 className="card-title text-white ">Iniciar sesión</h2>
                 <button
                   type="button"
                   className="btn btn-outline-secondary"
@@ -70,40 +82,44 @@ function Login() {
                   <MdCancel className="text-white" size={24} />
                 </button>
               </div>
+
               <form onSubmit={handleSubmit}>
                 <div className="mb-4">
                   <label
                     htmlFor="email"
-                    className="form-label text-warning yellow-600"
+                    className={`form-label ${
+                      errors.email ? "text-danger" : "text-warning "
+                    }`}
                   >
-                    Email
+                    Email {errors.email && <span>*</span>}
                   </label>
                   <input
                     aria-label="Email"
                     type="email"
-                    className="form-control "
+                    className={`form-control ${
+                      errors.email ? "is-invalid" : ""
+                    }`}
                     name="email"
                     value={formData.email}
                     onChange={handleChange}
                   />
-                  {errors.email && (
-                    <span className="text-danger" role="alert">
-                      {errors.email}
-                    </span>
-                  )}
                 </div>
                 <div className="mb-4">
                   <label
                     htmlFor="password"
-                    className="form-label text-warning yellow-600"
+                    className={`form-label ${
+                      errors.password ? "text-danger" : "text-warning"
+                    }`}
                   >
-                    Contraseña
+                    Contraseña {errors.password && <span>*</span>}
                   </label>
                   <div className="input-group">
                     <input
                       aria-label="Contraseña"
                       type={showPassword ? "text" : "password"}
-                      className="form-control"
+                      className={`form-control ${
+                        errors.password ? "is-invalid" : ""
+                      }`}
                       name="password"
                       value={formData.password}
                       onChange={handleChange}
@@ -117,23 +133,19 @@ function Login() {
                       <FaEye />
                     </button>
                   </div>
-                  {errors.password && (
-                    <span className="text-danger" role="alert">
-                      {errors.password}
-                    </span>
-                  )}
                 </div>
                 {errors.login && (
                   <div className="alert alert-danger" role="alert">
                     {errors.login}
                   </div>
                 )}
+                {/* Boton */}
                 <button type="submit" className="btn btn-warning yellow-600">
                   Iniciar sesión
                 </button>
                 <div className="mt-2">
                   <span className="text-white">
-                    Si no tienes una cuenta, <a href="/signup">regístrate</a>
+                    Si no tienes una cuenta, <a href="#/signup">regístrate</a>
                   </span>
                 </div>
               </form>
