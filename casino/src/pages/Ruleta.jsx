c38562c4ac994f8ba0897ea75658e762388bd3a7
Marta <martagranado1011@uma.es>
2024-05-27 17:45:20 +0200

    sonido

--- casino/src/pages/Ruleta.jsx
+++ casino/src/pages/Ruleta.jsx
@@ -7,6 +7,7 @@ import mp3Sound from "../assets/sound.mp3";
 import cheerx21 from "../assets/cheerx21.mp3";
 import FAQ from "../components/FAQ.jsx";
 import "../styles/Ruleta.css";
+import x11sound from "../assets/x11sound.mp3";
 
 import ruleta from "../assets/ruleta.png";
 import estrella from "../assets/estrella.png";
@@ -17,7 +18,7 @@ import x11Image from "../assets/x11.png";
 import x21Image from "../assets/x21.png";
 
 const Ruleta = () => {
-  const { fetchWalletBalance } = useWallet(); // Usa el hook del contexto para obtener la función de actualización del monedero
+  const { fetchWalletBalance } = useWallet(); 
   const [rotationDegrees, setRotationDegrees] = useState(0);
   const [selectedNumber, setSelectedNumber] = useState(0);
   const [numberSpins, setNumberSpins] = useState(1);
@@ -25,7 +26,7 @@ const Ruleta = () => {
   const userId = localStorage.getItem("userId");
 
   const [audio] = useState(new Audio(mp3Sound));
-  const [showX2, setShowX2] = useState(false); // New state to handle the x2 image display
+  const [showX2, setShowX2] = useState(false); 
   const [showX4, setShowX4] = useState(false);
   const [showX6, setShowX6] = useState(false);
   const [showX11, setShowX11] = useState(false);
@@ -77,7 +78,7 @@ const Ruleta = () => {
     const response = await fetchWalletBalance(userId);
     if (!response || !response.data || response.data.amount === undefined) {
       console.error("Invalid response format:", response);
-      return; // Exit the function if the response is invalid
+      return; 
     }
     const walletAmount = response.data.amount;
     if (index < 0 || index > 5) {
@@ -89,7 +90,7 @@ const Ruleta = () => {
       }
     } else {
       const newAmounts = [...amounts];
-      const parsedValue = Math.max(0, parseInt(value, 10)); // Prevent negative values
+      const parsedValue = Math.max(0, parseInt(value, 10)); 
       newAmounts[index] = isNaN(parsedValue) ? 0 : parsedValue;
       setAmounts(newAmounts);
   
@@ -153,22 +154,34 @@ const Ruleta = () => {
     const response = await fetchWalletBalance(userId);
     const currentBalance = response.data.amount;
 
-     // Calcula el nuevo saldo del monedero
     const newBalance = currentBalance + profit - lose;
 
-    // Actualiza el saldo en el servidor
     try {
       await axios.post("http://localhost:8081/walletUpdate", {
         money: newBalance,
         id: userId,
       });
 
-      // Actualiza el saldo en el contexto solo después de que se actualiza en el servidor correctamente
       fetchWalletBalance(userId);
     } catch (error) {
       console.error("Error updating wallet balance:", error);
     }
   };
+
+  useEffect(() => {
+    const handleSpaceKeyDown = (event) => {
+      if (event.code === "Space" && !girando && !showX2 && !showX4 && !showX6 && !showX11 && !showX21) {
+        event.preventDefault(); // Prevent the default behavior of the space key press
+        rouletteFunctioning();
+      }
+    };
+
+    window.addEventListener("keydown", handleSpaceKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleSpaceKeyDown);
+    };
+  }, [girando, showX2, showX4, showX6, showX11, showX21]);
 
   useEffect(() => {
     if (girando) {
@@ -179,21 +192,27 @@ const Ruleta = () => {
           setTimeout(() => {
             setShowX2(false);
             moneyGained(amounts);
-          }, 3000); // Display the x2 image for 3 seconds
+          }, 1500); 
         } else if (selectedNumber === 4) {
           setShowX4(true);
           setTimeout(() => {
             setShowX4(false);
             moneyGained(amounts);
-          }, 3000);
+          }, 1500);
         } else if (selectedNumber === 6) {
           setShowX6(true);
           setTimeout(() => {
             setShowX6(false);
             moneyGained(amounts);
-          }, 3000);
+          }, 1500);
         } else if (selectedNumber === 11) {
           setShowX11(true);
+          if (amounts[3] > 0) { 
+            const x11Audio = new Audio(x11sound);
+            x11Audio.play().catch((error) => {
+              console.error("Failed to play the x11 audio:", error);
+            });
+          }
           setTimeout(() => {
             setShowX11(false);
             moneyGained(amounts);
@@ -213,13 +232,12 @@ const Ruleta = () => {
         } else {
           moneyGained(amounts);
         }
-      }, 5100); // Espera 5 segundos, igual que la duración de la transición
+      }, 5100); 
       return () => clearTimeout(transitionTimer);
     }
   }, [girando]);
 
   useEffect(() => {
-    // Cleanup the audio when the component unmounts
     return () => {
       audio.pause();
       audio.currentTime = 0;
@@ -228,179 +246,176 @@ const Ruleta = () => {
 
   return (
     <>
-    <div style = {{backgroundColor: "#282828"}}>
-      <Navbar />
-      <div className="ruleta-page container">
-        <div className="faq-container">
-          <FAQ
-            FAQname={"¿CÓMO JUGAR?"}
-            FAQdescription={
-              "HAZ TUS APUESTAS EN LOS CAMPOS ABAJO<br/>" +
-              "CADA MULTIPLICADOR, MULTIPLICARÁ EL DINERO APOSTADO POR DICHO NÚMERO<br/>" +
-              "DALE A GIRAR A LA RULETA"
-            }
-            FAQindex={6}
-          />
-        </div>
-        <div className="row main-container">
-          <div className="col-12 col-lg-6 d-flex justify-content-center">
-            <div className="roulette-container position-relative">
-              <img
-                className="roulette img-fluid"
-                src={ruleta}
-                style={{
-                  transform: `rotate(${rotationDegrees}deg)`,
-                  transition: "transform 5s ease",
-                }}
-                alt="Ruleta"
-              />
-              <img
-                className={`star position-absolute ${girando || showX2 || showX4 || showX6 || showX11 || showX21 ? "disabled" : ""}`}
-                src={estrella}
-                alt="Overlay"
-                onClick={!girando && !showX2 && !showX4 && !showX6 && !showX11 && !showX21 ? rouletteFunctioning : undefined}
-                aria-label="Girar ruleta"
-                role="button"
-              />
+      <div style = {{backgroundColor: "#282828"}}>
+        <Navbar />
+        <div className="ruleta-page container">
+          <div className="faq-container">
+            <FAQ
+              FAQname={"¿CÓMO JUGAR?"}
+              FAQdescription={
+                "HAZ TUS APUESTAS EN LOS CAMPOS ABAJO<br/>" +
+                "CADA MULTIPLICADOR, MULTIPLICARÁ EL DINERO APOSTADO POR DICHO NÚMERO<br/>" +
+                "DALE A GIRAR A LA RULETA"
+              }
+              FAQindex={6}
+            />
+          </div>
+          <div className="row main-container">
+            <div className="col-12 col-lg-6 d-flex justify-content-center">
+              <div className="roulette-container position-relative">
+                <img
+                  className="roulette img-fluid"
+                  src={ruleta}
+                  style={{
+                    transform: `rotate(${rotationDegrees}deg)`,
+                    transition: "transform 5s ease",
+                  }}
+                  alt="Ruleta"
+                />
+                <img
+                  className={`star position-absolute ${girando || showX2 || showX4 || showX6 || showX11 || showX21 ? "disabled" : ""}`}
+                  src={estrella}
+                  alt="Overlay"
+                  onClick={!girando && !showX2 && !showX4 && !showX6 && !showX11 && !showX21 ? rouletteFunctioning : undefined}
+                  aria-label="Girar ruleta"
+                  role="button"
+                />
 
-              {showX2 && (
-                <img
-                  src={x2Image}
-                  alt="Por 2"
-                  className="position-absolute pulsate non-clickable"
-                  style={{
-                    marginBottom: "100%",
-                    marginLeft: "50%"
-                  }}
-                  aria-label="multiplicado por 2"
-                />
+                {showX2 && (
+                  <img
+                    src={x2Image}
+                    alt="Por 2"
+                    className="position-absolute pulsate non-clickable"
+                    style={{
+                      marginBottom: "100%",
+                      marginLeft: "50%"
+                    }}
+                    aria-label="multiplicado por 2"
+                  />
+                )}
+                {showX4 && (
+                  <img
+                    src={x4Image}
+                    alt="Por 4"
+                    className="position-absolute pulsate non-clickable"
+                    style={{
+                      marginBottom: "100%",
+                      marginLeft: "50%"
+                    }}
+                    aria-label="multiplicado por 4"
+                  />
+                )}
+                {showX6 && (
+                  <img
+                    src={x6Image}
+                    alt="Por 6"
+                    className="position-absolute pulsate non-clickable"
+                    style={{
+                      marginBottom: "100%",
+                      marginLeft: "50%"
+                    }}
+                    aria-label="multiplicado por 6"
+                  />
+                )}
+                {showX11 && (
+                  <img
+                    src={x11Image}
+                    alt="Por 11"
+                    className="position-absolute pulsate non-clickable"
+                    style={{
+                      marginBottom: "100%",
+                      marginLeft: "50%"
+                    }}
+                    aria-label="multiplicado por 11"
+                  />
+                )}
+                {showX21 && (
+                  <img
+                    src={x21Image}
+                    alt="Por 21"
+                    className="position-absolute pulsate non-clickable"
+                    style={{
+                      marginBottom: "100%",
+                      marginLeft: "50%"
+                    }}
+                    aria-label="multiplicado por 21"
+                  />
+                )}
+              </div>
+            </div>
+            <div className="col-12 col-lg-6 bets-container d-flex flex-column align-items-center align-items-lg-start">
+              {amounts.map((value, index) => (
+                <div key={index} className="bet-input mb-3">
+                  <label htmlFor={`bet-${index}`} className="text-white form-label">
+                    Apuestas al X{seccionesMap.get(index)}:
+                  </label>
+                  <div className="input-group">
+                    <input
+                      id={`bet-${index}`}
+                      type="number"
+                      className="form-control"
+                      value={parseInt(value)}
+                      onChange={(e) => handleAmountChange(index, e.target.value)}
+                      aria-label={`Apuestas al X${seccionesMap.get(index)}`}
+                      min="0" 
+                      disabled={girando || showX2 || showX4 || showX6 || showX11 || showX21} 
+                    />
+
+                    <div className="input-group-append">
+                      <button
+                        className="btn btn-outline-secondary bet-button"
+                        type="button"
+                        onClick={() => handleAmountChange(index, 1)}
+                        disabled={girando || showX2 || showX4 || showX6 || showX11 || showX21} 
+                      >
+                        1€
+                      </button>
+                      <button
+                        className="btn btn-outline-secondary bet-button"
+                        type="button"
+                        onClick={() => handleAmountChange(index, 5)}
+                        disabled={girando || showX2 || showX4 || showX6 || showX11 || showX21} 
+                      >
+                        5€
+                      </button>
+                      <button
+                        className="btn btn-outline-secondary bet-button"
+                        type="button"
+                        onClick={() => handleAmountChange(index, 10)}
+                        disabled={girando || showX2 || showX4 || showX6 || showX11 || showX21} 
+                      >
+                        10€
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              {insufficientFunds && (
+                <div style={{ color: "red" }} aria-label="Fondos insuficientes">
+                  ¡Fondos Insuficientes!
+                </div>
               )}
-              {showX4 && (
-                <img
-                  src={x4Image}
-                  alt="Por 4"
-                  className="position-absolute pulsate non-clickable"
-                  style={{
-                    marginBottom: "100%",
-                    marginLeft: "50%"
-                  }}
-                  aria-label="multiplicado por 4"
-                />
-              )}
-              {showX6 && (
-                <img
-                  src={x6Image}
-                  alt="Por 6"
-                  className="position-absolute pulsate non-clickable"
-                  style={{
-                    marginBottom: "100%",
-                    marginLeft: "50%"
-                  }}
-                  aria-label="multiplicado por 6"
-                />
-              )}
-              {showX11 && (
-                <img
-                  src={x11Image}
-                  alt="Por 11"
-                  className="position-absolute pulsate non-clickable"
-                  style={{
-                    marginBottom: "100%",
-                    marginLeft: "50%"
-                  }}
-                  aria-label="multiplicado por 11"
-                />
-              )}
-              {showX21 && (
-                <img
-                  src={x21Image}
-                  alt="Por 21"
-                  className="position-absolute pulsate non-clickable"
-                  style={{
-                    marginBottom: "100%",
-                    marginLeft: "50%"
-                  }}
-                  aria-label="multiplicado por 21"
-                />
-              )}
+              <button
+                className="btn btn-primary mt-3"
+                onClick={rouletteFunctioning}
+                disabled={insufficientFunds || girando || showX2 || showX4 || showX6 || showX11 || showX21} 
+                aria-label="Girar ruleta"
+              >
+                Girar ruleta
+              </button>
+              <p
+                className="text-white mt-3"
+                aria-label={`Has ganado ${actualProfit}`}
+              >
+                Has Ganado: {actualProfit}
+              </p>
+              <p className="text-white" aria-label={`Has perdido ${actualLose}`}>
+                Has Perdido: {actualLose}
+              </p>
             </div>
           </div>
-          <div className="col-12 col-lg-6 bets-container d-flex flex-column align-items-center align-items-lg-start">
-  {amounts.map((value, index) => (
-    <div key={index} className="bet-input mb-3">
-      <label htmlFor={`bet-${index}`} className="text-white form-label">
-        Apuestas al X{seccionesMap.get(index)}:
-      </label>
-      <div className="input-group">
-        <input
-          id={`bet-${index}`}
-          type="number"
-          className="form-control"
-          value={parseInt(value)}
-          onChange={(e) => handleAmountChange(index, e.target.value)}
-          aria-label={`Apuestas al X${seccionesMap.get(index)}`}
-          min="0" // Set minimum value to zero
-          disabled={girando || showX2 || showX4 || showX6 || showX11 || showX21} // Disable input when spinning or multiplier image is showing
-        />
-
-        <div className="input-group-append">
-          <button
-            className="btn btn-outline-secondary bet-button"
-            type="button"
-            onClick={() => handleAmountChange(index, 1)}
-            disabled={girando || showX2 || showX4 || showX6 || showX11 || showX21} // Disable button when spinning or multiplier image is showing
-          >
-            1€
-          </button>
-          <button
-            className="btn btn-outline-secondary bet-button"
-            type="button"
-            onClick={() => handleAmountChange(index, 5)}
-            disabled={girando || showX2 || showX4 || showX6 || showX11 || showX21} // Disable button when spinning or multiplier image is showing
-          >
-            5€
-          </button>
-          <button
-            className="btn btn-outline-secondary bet-button"
-            type="button"
-            onClick={() => handleAmountChange(index, 10)}
-            disabled={girando || showX2 || showX4 || showX6 || showX11 || showX21} // Disable button when spinning or multiplier image is showing
-          >
-            10€
-          </button>
         </div>
-      </div>
-    </div>
-  ))}
-  {insufficientFunds && (
-    <div style={{ color: "red" }} aria-label="Fondos insuficientes">
-      ¡Fondos Insuficientes!
-    </div>
-  )}
-  <button
-    className="btn btn-primary mt-3"
-    onClick={rouletteFunctioning}
-    disabled={insufficientFunds || girando || showX2 || showX4 || showX6 || showX11 || showX21} // Updated condition
-    aria-label="Girar ruleta"
-  >
-    Girar ruleta
-  </button>
-  <p
-    className="text-white mt-3"
-    aria-label={`Has ganado ${actualProfit}`}
-  >
-    Has Ganado: {actualProfit}
-  </p>
-  <p className="text-white" aria-label={`Has perdido ${actualLose}`}>
-    Has Perdido: {actualLose}
-  </p>
-</div>
-
-
-
-        </div>
-      </div>
-      <Footer />
+        <Footer />
       </div>
     </>
   );
