65fcd669a21ed7e8dc539dd8412bf67416662385
David Zarzavilla Borrego <dzarzavillaborrego@uma.es>
2024-06-05 18:49:51 +0200

    Update copy-404.js

--- casino/copy-404.js
+++ casino/copy-404.js
@@ -1,8 +1,12 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Obtener __dirname en un módulo ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Define las rutas del archivo fuente y destino
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const sourcePath = path.join(__dirname, '404.html');
 const destinationPath = path.join(__dirname, 'dist', '404.html');
 
